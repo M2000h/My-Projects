c9eef7b85795cef6a33fca2f17f99e0eae722367
Maxim Shakura <43370239+M2000h@users.noreply.github.com>
2019-02-26 15:34:05 +0300

    Update Snake.cpp

--- Console Snake/programm/Snake.cpp
+++ Console Snake/programm/Snake.cpp
@@ -8,13 +8,22 @@ using namespace std;
 HANDLE hOCol = GetStdHandle(STD_OUTPUT_HANDLE);
 #define BOARD_SIZE 10
 
+#define SNAKE_BODY_CHAR 219
+#define SNAKE_HEAD_CHAR 1
+#define COOKIE_CHAR 15
+#define BACK_GROUND_CHAR 0
+
+#define SNAKE_BODY_COLOR 250
+#define SNAKE_HEAD_COLOR 252
+#define COOKIE_COLOR 254
+#define BACK_GROUND_COLOR 250
+
 int start();
-void stop(int ShakeLenght);
+void stop(int SnakeLenght);
 bool check();
-void print(int ShakeLenght);
+void print(POINT *SnakeBody, POINT Cookie,int SnakeLenght);
 void gotoxy(int x, int y,int color, int letter);
 
-POINT p[100], Cookie;
 void main()
 {
 	system("title SNAKE");
@@ -28,15 +37,16 @@ void main()
 }
 
 int start() {
-	SetConsoleTextAttribute(hOCol, 249);
+	POINT SnakeBody[100], Cookie;
+	SetConsoleTextAttribute(hOCol, BACK_GROUND_COLOR);
 	system("cls");
-	int  ShakeLenght = 3;
+	int SnakeLenght = 3;
 	int n = 100;
 	//cout << "your speed (recomended 50): ";
 	//cin >> n;
 	//system("cls");
-	p[0].x = 0;
-	p[0].y = 0;
+	SnakeBody[0].x = 0;
+	SnakeBody[0].y = 0;
 	Cookie.x = rand() % BOARD_SIZE;
 	Cookie.y = rand() % BOARD_SIZE;
 	int direction = 0,
@@ -46,54 +56,56 @@ int start() {
 		if (_kbhit())
 		{
 			_getch();
-			q = _getch();
-			if (q != 72 && q != 80 && q != 77 && q != 75)
-				q = direction;
+			if (_kbhit()) {
+				q = _getch();
+				if (q != 72 && q != 80 && q != 77 && q != 75)
+					q = direction;
+			}
 		}
-		//for (a = 0;a < 2;a++)
-		//{
-		if (Cookie.x == p[0].x && Cookie.y == p[0].y)
+
+
+		if (Cookie.x == SnakeBody[0].x && Cookie.y == SnakeBody[0].y)
 		{
-			ShakeLenght++;
+			SnakeLenght++;
 			Cookie.x = rand() % BOARD_SIZE;
 			Cookie.y = rand() % BOARD_SIZE;
 		}
-		//}
+		
 
-		for (int a = ShakeLenght; a > 0; a--)
+		for (int a = SnakeLenght; a > 0; a--)
 		{
-			p[a].x = p[a - 1].x;
-			p[a].y = p[a - 1].y;
+			SnakeBody[a].x = SnakeBody[a - 1].x;
+			SnakeBody[a].y = SnakeBody[a - 1].y;
 		}
 		//начало стрелок
 		if (abs(q - direction) == 8 || abs(q - direction) == 2)
 			q = direction;//chek1
 		switch (q) {
 		case 72://вниз
-			p[0].y = p[0].y == 0 ? BOARD_SIZE - 1 : p[0].y - 1;
+			SnakeBody[0].y = SnakeBody[0].y == 0 ? BOARD_SIZE - 1 : SnakeBody[0].y - 1;
 			break;
 		case 80://вверх
-			p[0].y = p[0].y == BOARD_SIZE - 1 ? 0 : p[0].y + 1;
+			SnakeBody[0].y = SnakeBody[0].y == BOARD_SIZE - 1 ? 0 : SnakeBody[0].y + 1;
 			break;
 		case 77://впрао
-			p[0].x = p[0].x == BOARD_SIZE - 1 ? 0 : p[0].x + 1;
+			SnakeBody[0].x = SnakeBody[0].x == BOARD_SIZE - 1 ? 0 : SnakeBody[0].x + 1;
 			break;
 		case 75://влево
-			p[0].x = p[0].x == 0 ? BOARD_SIZE - 1 : p[0].x - 1;
+			SnakeBody[0].x = SnakeBody[0].x == 0 ? BOARD_SIZE - 1 : SnakeBody[0].x - 1;
 			break;
 		}
-		if (q == 72 || q == 80 || q == 77 || q == 75)
-			direction = q;
+		
+		direction = q;
 		//конец стрелок
 		
-		for (int a = 1; a < ShakeLenght - 1 && ShakeLenght > 4; a++)
-			if (p[0].x == p[a].x && p[0].y == p[a].y)
-				return ShakeLenght;
+		for (int a = 1; a < SnakeLenght - 1 && SnakeLenght > 4; a++)
+			if (SnakeBody[0].x == SnakeBody[a].x && SnakeBody[0].y == SnakeBody[a].y)
+				return SnakeLenght;
 
-		if (ShakeLenght < 20)
-			n = 100 - ShakeLenght * 4;
+		if (SnakeLenght < 20)
+			n = 100 - SnakeLenght * 4;
 		Sleep(n);
-		print(ShakeLenght);
+		print(SnakeBody,Cookie, SnakeLenght);
 	}
 }
 
@@ -113,11 +125,10 @@ bool check() {
 	}
 }
 
-
-void stop(int ShakeLenght) {
+void stop(int SnakeLenght) {
 	system("cls");
 	SetConsoleTextAttribute(hOCol, 249);
-	printf("END\nSCORE - %d\n", ShakeLenght);
+	printf("END\nSCORE - %d\n", SnakeLenght);
 	
 	SetConsoleTextAttribute(hOCol, 250);
 	printf("TO PLAY AGAIN - PRESS ENTER\n");
@@ -127,28 +138,26 @@ void stop(int ShakeLenght) {
 }
 
 
-void print(int ShakeLenght)
+void print(POINT *SnakeBody, POINT Cookie,int SnakeLenght)
 {
-	gotoxy(2 * p[1].x, 2 * p[1].y, 250, 219); //переход тулвищав голову
+	gotoxy(SnakeBody[1].x, SnakeBody[1].y, SNAKE_BODY_COLOR, SNAKE_BODY_CHAR); //переход тулвищав голову
+	 
+	gotoxy(SnakeBody[SnakeLenght - 1].x, SnakeBody[SnakeLenght - 1].y, BACK_GROUND_COLOR, 0); //удаление жопы
 
-	gotoxy(2 * p[ShakeLenght - 1].x, 2 * p[ShakeLenght - 1].y, 250, 0); //удаление жопы
+	gotoxy(Cookie.x, Cookie.y, COOKIE_COLOR, COOKIE_CHAR);//печеньак
 
-	gotoxy(2 * Cookie.x, 2 * Cookie.y, 254, 15);//печеньак
+	gotoxy(SnakeBody[0].x,SnakeBody[0].y, SNAKE_HEAD_COLOR, SNAKE_HEAD_CHAR);//прихерачиваниеголовы
 
-	gotoxy(2 * p[0].x, 2 * p[0].y, 252, 1);//прихерачиваниеголовы
-
-	gotoxy(2 * BOARD_SIZE, 2 * BOARD_SIZE, 250, 0);
+	gotoxy(BOARD_SIZE,BOARD_SIZE, BACK_GROUND_COLOR, BACK_GROUND_CHAR);
 }
 
 
-void gotoxy(int x, int y,int color, int letter)
+void gotoxy(int x, int y, int color, int letter)
 {
 	SetConsoleTextAttribute(hOCol, color);
-	COORD  coord = { x, y };
-	SetConsoleCursorPosition(hOCol, coord);
+	SetConsoleCursorPosition(hOCol, { (short)(2 * x), (short)(2 * y) });
 	cout << (char)letter << (char)letter;
 
-	COORD  coord1 = { x, y+1 };
-	SetConsoleCursorPosition(hOCol, coord1);
+	SetConsoleCursorPosition(hOCol, { (short)(2 * x), (short)(2 * y + 1) });
 	cout << (char)letter << (char)letter;
 }
